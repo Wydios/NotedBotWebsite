cc43b8aefcbf61bb7238c441f3ca7a788604960e
Wydios <fabianhorter@gmail.com>
2024-08-03 10:54:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const starfield = document.querySelector('.starfield');
+    const numStars = 100;
+
+    for (let i = 0; i < numStars; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.left = `${Math.random() * 100}%`;
+        starfield.appendChild(star);
+    }
+});
