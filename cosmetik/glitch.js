d7c27c1e235dcf5ad8d8fd1452bf87abbd1267ac
Wydios <fabianhorter@gmail.com>
2024-12-17 12:10:13 +0100

    Update glitch.js

--- cosmetik/glitch.js
+++ cosmetik/glitch.js
@@ -1,13 +1,19 @@
-// kosmetik/glitch.js
-export function applyGlitchEffect(element) {
-    const glitchEffect = () => {
-        element.classList.add('glitch');
-    };
-
-    const removeGlitchEffect = () => {
-        element.classList.remove('glitch');
-    };
-
-    element.addEventListener('mouseover', glitchEffect);  
-    element.addEventListener('mouseout', removeGlitchEffect);  
-}
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.PowerGlitch) {
+        const glitchEffect = window.PowerGlitch.glitch;
+        const logo = document.getElementById("notedbot-logo");
+        logo.addEventListener("mouseenter", () => {
+            glitchEffect("#notedbot-logo", { playMode: "hover" }).startGlitch();
+        });
+        logo.addEventListener("mouseleave", () => {
+            glitchEffect("#notedbot-logo", { playMode: "hover" }).stopGlitch();
+        });
+        glitchEffect("#notedbot-logo", {
+            playMode: "hover",
+            shake: { velocity: 20, amplitudeX: 0.3, amplitudeY: 0.3 },
+            slice: { count: 10, velocity: 25 }
+        }).startGlitch();
+    } else {
+        console.error("PowerGlitch konnte nicht gefunden werden!");
+    }
+});
