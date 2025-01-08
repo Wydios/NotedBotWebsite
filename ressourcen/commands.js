91202a589e238fecd93866fb922fb9c27ea9b115
Wydios <fabianhorter@gmail.com>
2025-01-08 17:19:11 +0100

    Create commands.js

---
+++ ressourcen/commands.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+        name: 'vanity',
+        aliases: ['style'],
+        descriptionDE: 'Schickt einen Link zu einer Seite, wo du deinen Style sehen kannst.',
+        descriptionUS: 'Sends a link to a page where you can see your style.',
+        usageDE: 'Schreibe !vanity [username]',
+        usageUS: 'Write !vanity [username]',
+        category: 'Normal',
+    }
+];
