24e2e14f701399833f3fd541aaa4e52a22cca916
Wydios <fabianhorter@gmail.com>
2025-01-08 17:18:16 +0100

    Create commandsexport.js

---
+++ ressourcen/commandsexport.js
@@ -0,0 +1,19 @@
+const commands = require('./commands'); 
+
+const commandsDE = commands.map(cmd => ({
+    name: cmd.name,
+    aliases: cmd.aliases,
+    description: cmd.descriptionDE, 
+    usage: cmd.usageDE,             
+    category: cmd.category,
+}));
+
+const commandsUS = commands.map(cmd => ({
+    name: cmd.name,
+    aliases: cmd.aliases,
+    description: cmd.descriptionUS,
+    usage: cmd.usageUS,            
+    category: cmd.category,
+}));
+
+module.exports = { commandsDE, commandsUS, };
