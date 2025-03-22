6d732d38c4b3884b032bd68adb76e79d464bfcde
Wydios <fabianhorter@gmail.com>
2025-03-22 18:38:49 +0100

    Updatet Badge

--- ressourcen/7tv/badge.js
+++ ressourcen/7tv/badge.js
@@ -1,28 +1,66 @@
 function getUrlParams() {
     const urlParams = new URLSearchParams(window.location.search);
     const badgeID = urlParams.get('badgeID');
-    const badgeName = urlParams.get('badgeName');
-    return { badgeID, badgeName };
-};
+    return { badgeID };
+}
 
-function loadBadge() {
-    const { badgeID, badgeName } = getUrlParams();
-    const badgeImage = document.getElementById('badge-image');
-    const badgeNameElement = document.getElementById('badge-name');
+function getBadge() {
+    const { badgeID } = getUrlParams();
+    const query = `
+        query Paints {
+            badges {
+                badges {
+                    id
+                    name
+                    description
+                        images {
+                            url 
+                            mime
+                            size
+                            scale
+                            width
+                            height
+                            frameCount
+                        }
+                    }
+                }
+            }
+        `;
+        fetch('https://7tv.io/v4/gql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query: query }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.data && data.data.badges && data.data.badges.badges) {
+                const badgeData = data.data.badges.badges.find(badge => badge.id === badgeID);
+                if (badgeData) {
+                    console.log(`Badge Daten für ${badgeData.name} ID: ${badgeID} ->`);
+                    console.log(JSON.stringify(badgeData, null, 2));
 
-    if (badgeImage) {
-        if (badgeID) {
-            badgeImage.src = `https://cdn.7tv.app/badge/${badgeID}/4x.avif`;
-            badgeImage.style.display = "block";
+                    const badgeElement = document.getElementById('badge-image');
+                    const badgeName = document.getElementById('badge-name');
+
+                    if (badgeElement && badgeName) {
+                        badgeName.textContent = badgeData.name;
+                        document.title = `NotedBot │ 7TV ${badgeData.name} Badge`;
+                        badgeElement.src = `https://cdn.7tv.app/badge/${badgeID}/4x.avif`;
+                        badgeElement.style.display = "block";
+                    }
+            } else {
+                console.error('Keine Badge Daten gefunden für ID:', badgeID);
+            }
+        } else {
+            console.error('Keine Badge Daten gefunden');
         }
-    } else {
-        console.error('Das Badge-Bild-Element konnte nicht gefunden werden.');
-    }
-
-    if (badgeName) {
-        badgeNameElement.textContent = badgeName;
-        document.title = `NotedBot │ 7TV ${badgeName} Badge`;
-    }
+    })
+    .catch(error => {
+        console.error('getBadge | Fehler beim fetchen vom Badges', error);
+    });
 };
 
-window.onload = loadBadge;
+getBadge();
